198d71dcc9fea267df9ee6a7e89f8e0958fee6e3
Diego Cavalcanti <diego.scavalcanti@gmail.com>
2024-09-25 16:25:06 -0300

    feat: cards containers

--- app/src/main/java/com/example/mavidsmileapp/MainActivity.kt
+++ app/src/main/java/com/example/mavidsmileapp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.mavidsmileapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val containerOne = findViewById<LinearLayout>(R.id.containerOne)
+        val containerTwo = findViewById<LinearLayout>(R.id.containerTwo)
+        val nextButton = findViewById<ImageButton>(R.id.nextButton)
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+
+        // Inicialmente exibe o primeiro card
+        containerOne.visibility = View.VISIBLE
+        containerTwo.visibility = View.GONE
+
+        // Ao clicar no botão "setButton", troca para o segundo card
+        nextButton.setOnClickListener {
+            containerOne.visibility = View.GONE
+            containerTwo.visibility = View.VISIBLE
+        }
+
+        // Ao clicar no botão "backButton", volta para o primeiro card
+        backButton.setOnClickListener {
+            containerTwo.visibility = View.GONE
+            containerOne.visibility = View.VISIBLE
+        }
+
+
+
     }
 }
